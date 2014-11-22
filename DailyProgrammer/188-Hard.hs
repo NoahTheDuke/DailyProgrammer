dbb5c75456df9ea6ef8f021bf243267b28a01994
NoahTheDuke <nbtheduke@gmail.com>
2014-11-22 03:34:42 -0500

    Successfully implemented the maze generator.

--- DailyProgrammer/188-Hard.hs
+++ DailyProgrammer/188-Hard.hs
@@ -13,10 +13,18 @@ height = 20
 text :: String
 text = show width ++ " " ++ show height
 
+insertAt :: Int -> [a] -> [a] -> [a]
+insertAt i p [] = []
+insertAt i p a =
+    let splitted = splitAt i a
+        first = fst $ splitted
+        second = insertAt i p $ snd $ splitted
+    in first ++ p ++ second
+
 main :: IO ()
 main = do
     putStrLn text
     g <- newStdGen
-    let ns = take width $ randomRs (0, length directions-1) g
-        strs = map (directions !!) ns
-    putStrLn $ show strs
+    let strs = insertAt width "\n" $ concatMap (directions !!) $ take (width * height) $ randomRs (0, length directions-1) g
+    writeFile (show width ++ "-" ++ show height ++ ".txt") $ text ++ "\n" ++ strs
+    putStr strs
