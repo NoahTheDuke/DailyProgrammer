0800efcb1a48e2d20de281ec4f9c7ab06ba41733
NoahTheDuke <nbtheduke@gmail.com>
2015-01-14 11:32:05 -0500

    197-E, py: Started, and finished! Fun little one.

--- Python/Problems/195-Intermediate.py
+++ Python/Problems/195-Intermediate.py
@@ -9,12 +9,10 @@ discard, target_die = target_die.split('d')
 num_of_sc_dice, type_of_sc_dice = scoring_dice.split('d')
 num_of_sc_dice, type_of_sc_dice = int(num_of_sc_dice), int(type_of_sc_dice)
 target = random.randrange(1, int(target_die) + 1)
-potentials = []
-potentials = [random.randrange(1, type_of_sc_dice + 1) for x in range(num_of_sc_dice)]
+dice_shown = []
+dice_shown = [random.randrange(1, type_of_sc_dice + 1) for x in range(num_of_sc_dice)]
 print("1d{} sets the Target to {}.".format(target_die, target))
-print("{}d{} produces {} to work with.".format(num_of_sc_dice, type_of_sc_dice, potentials))
-
-
+print("{}d{} produces {} to work with.".format(num_of_sc_dice, type_of_sc_dice, dice_shown))
 
 solution = "Figure it out, nerd."
 print("One solution is: {}".format(solution))
