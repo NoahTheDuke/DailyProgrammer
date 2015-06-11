08565420ef0874d128ee19ccb053ce0dce82bb68
NoahTheDuke <nbtheduke@gmail.com>
2015-06-11 01:15:40 -0400

    Additions from Monday.

--- Python/Problems/216-Intermediate.py
+++ Python/Problems/216-Intermediate.py
@@ -1,16 +1,12 @@
 import random
+import collections as co
+import itertools as it
 
-class Player:
-    #def __init__(self, position, starting_money):
-        #self.position = position
-        #self.money = starting_money
-    def __init__(self, name, hand):
-        self.name = name
-        self.hand = hand
+Player = co.namedtuple('Player', ['name', 'hand'])
 
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
 suits = ['Hearts', 'Spades', 'Diamonds', 'Clubs']
-deck = ['{} of {}'.format(*card) for card in ((x,y) for x in values for y in suits)]
+deck = [(x,y) for x in values for y in suits]
 
 random.shuffle(deck)
 
@@ -22,11 +18,21 @@ players = []
 players.append(Player("Human", deal(2)))
 for i in range(num_players - 1):
     players.append(Player("CPU {}".format(i + 1), deal(2)))
+flop, turn, river = deal(3), deal(1), deal(1)
+board = flop + turn + river
+
+# Poker hands:
+# High card, one pair, two pair,
+# three of a kind, straight, flush,
+# full house, four of a kind, straight flush
+def generate_hand(player_hand):
+    test = player_hand.hand + board
+    #straight flush
+    return it.permutations(test, 5)
 
 for player in players:
     print("{}: {}, {}".format(player.name, *player.hand))
 
-flop, turn, river = deal(3), deal(1), deal(1)
 print("Flop: {}, {}, {}".format(*flop))
 print("Turn: {}".format(*turn))
 print("River: {}".format(*river))
