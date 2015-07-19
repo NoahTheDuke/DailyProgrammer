73bf27f0ef9c52a5bf528ed6fc4f287c41aeaa9a
NoahTheDuke <nbtheduke@gmail.com >
2015-07-18 23:42:19 -0400

    Beeminder commit

--- Python/Problems/216-Intermediate.py
+++ Python/Problems/216-Intermediate.py
@@ -38,6 +38,6 @@ print("Turn: {}".format(*turn))
 print("River: {}".format(*river))
 
 #def best_hand(blah blah):
-#   return "blah"
+#   return "blah."
 
 #print("Winner: {}, with a {}.".format(winner, best_hand))
